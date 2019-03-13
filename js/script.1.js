e704de8d47c56b7bb1d6b345d8a5ec6d6dc618ae
Emanuel Mustea <plus.emanuelmustea@gmail.com>
2019-03-13 11:44:30 +0200

    prettier code

--- js/script.1.js
+++ js/script.1.js
@@ -4,12 +4,8 @@ const rangeContainer = getElement(".custom-range");
 const rangeIndicatorContainer = getElement(".custom-range > .range-indicator");
 const leftThumb = getElement(".custom-range > .thumb.left");
 const rightThumb = getElement(".custom-range > .thumb.right");
-const leftIndicatorContainer = getElement(
-  ".custom-range > .value-indicator.left"
-);
-const rightIndicatorContainer = getElement(
-  ".custom-range > .value-indicator.right"
-);
+const leftIndicatorContainer = getElement(".custom-range > .value-indicator.left");
+const rightIndicatorContainer = getElement(".custom-range > .value-indicator.right");
 
 const rangeMinValue = parseInt(rangeContainer.getAttribute("data-min"));
 const rangeMaxValue = parseInt(rangeContainer.getAttribute("data-max"));
@@ -25,61 +21,44 @@ const removeListener = event => {
   document.removeEventListener("mousemove", calculateRightXPosition);
 };
 
-const calculateValue = thumbOffset =>
-  Math.ceil((thumbOffset * rangeMaxValue) / (rangeWidth - thumbWidth));
+const calculateValue = thumbOffset => Math.ceil((thumbOffset * rangeMaxValue) / (rangeWidth - thumbWidth));
 const calculateLeftXPosition = event => {
   const mouseOffsetX = event.clientX;
-  const thumbOffset =
-    mouseOffsetX - rangeContainer.getBoundingClientRect().left - thumbWidth / 2;
+  const thumbOffset = mouseOffsetX - rangeContainer.getBoundingClientRect().left - thumbWidth / 2;
   const value = calculateValue(thumbOffset);
-  if (
-    thumbOffset >= 0 &&
-    thumbOffset <= rangeContainer.offsetWidth - thumbWidth
-  ) {
+  if (thumbOffset >= 0 && thumbOffset <= rangeContainer.offsetWidth - thumbWidth) {
     leftThumb.style.left = `${thumbOffset}px`;
     if (thumbOffset < rightThumb.offsetLeft) {
       rangeIndicatorContainer.style.left = `${thumbOffset}px`;
-      rangeIndicatorContainer.style.width = `${rightThumb.offsetLeft -
-        thumbOffset}px`;
+      rangeIndicatorContainer.style.width = `${rightThumb.offsetLeft - thumbOffset}px`;
     } else {
       rangeIndicatorContainer.style.left = `${rightThumb.offsetLeft}px`;
-      rangeIndicatorContainer.style.width = `${thumbOffset -
-        rightThumb.offsetLeft}px`;
+      rangeIndicatorContainer.style.width = `${thumbOffset - rightThumb.offsetLeft}px`;
     }
     leftValue = value;
     leftIndicatorContainer.innerHTML = leftValue;
     rightIndicatorContainer.style.zIndex = 0;
-    leftIndicatorContainer.style.left = `${thumbOffset +
-      thumbWidth / 2 -
-      indicatorWidth / 2}px`;
+    leftIndicatorContainer.style.left = `${thumbOffset + thumbWidth / 2 - indicatorWidth / 2}px`;
   }
 };
 const calculateRightXPosition = event => {
   const mouseOffsetX = event.clientX;
-  const thumbOffset =
-    mouseOffsetX - rangeContainer.getBoundingClientRect().left - thumbWidth / 2;
+  const thumbOffset = mouseOffsetX - rangeContainer.getBoundingClientRect().left - thumbWidth / 2;
 
   const value = calculateValue(thumbOffset);
-  if (
-    thumbOffset >= 0 &&
-    thumbOffset <= rangeContainer.offsetWidth - thumbWidth
-  ) {
+  if (thumbOffset >= 0 && thumbOffset <= rangeContainer.offsetWidth - thumbWidth) {
     rightThumb.style.left = `${thumbOffset}px`;
     if (thumbOffset > leftThumb.offsetLeft) {
       rangeIndicatorContainer.style.left = `${leftThumb.offsetLeft}px`;
-      rangeIndicatorContainer.style.width = `${thumbOffset -
-        leftThumb.offsetLeft}px`;
+      rangeIndicatorContainer.style.width = `${thumbOffset - leftThumb.offsetLeft}px`;
     } else {
       rangeIndicatorContainer.style.left = `${thumbOffset}px`;
-      rangeIndicatorContainer.style.width = `${leftThumb.offsetLeft -
-        thumbOffset}px`;
+      rangeIndicatorContainer.style.width = `${leftThumb.offsetLeft - thumbOffset}px`;
     }
     rightValue = value;
     rightIndicatorContainer.innerHTML = value;
     rightIndicatorContainer.style.zIndex = 1;
-    rightIndicatorContainer.style.left = `${thumbOffset +
-      thumbWidth / 2 -
-      indicatorWidth / 2}px`;
+    rightIndicatorContainer.style.left = `${thumbOffset + thumbWidth / 2 - indicatorWidth / 2}px`;
   }
 };
 const instantMove = event => {
@@ -98,7 +77,6 @@ const instantMove = event => {
 };
 
 rangeContainer.addEventListener("mousedown", instantMove);
-
 document.addEventListener("mouseup", removeListener);
 
 rightIndicatorContainer.innerHTML = rightValue;
